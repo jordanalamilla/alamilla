9835dc3b6aa089a4ad292d6869e627338e7d1790
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2026-04-22 14:20:40 -0400

    undid some updates - changing my approach to developing this theme
    Switching from a hybrid block theme approach to more of a classic theme approach but centred on working with the built in blocks that wordpress already provides

--- functions.php
+++ functions.php
@@ -8,19 +8,12 @@
 /**
  * Alamilla child theme functions and definitions
  */
-function alamilla_child_enqueue_styles() {
-	wp_enqueue_style(
-		'twentytwentyfive-style',
-		get_template_directory_uri() . '/style.css',
-		array(),
-		wp_get_theme( get_template() )->get( 'Version' )
-	);
-
-	wp_enqueue_style(
-		'alamilla-style',
-		get_stylesheet_uri(),
-		array( 'twentytwentyfive-style' ),
-		wp_get_theme()->get( 'Version' )
-	);
+function alamilla_enqueue_styles() {
+		wp_enqueue_style(
+			'alamilla-style',
+			get_stylesheet_uri(),
+			array( 'twentytwentyfive-style' ),
+			wp_get_theme()->get( 'Version' )
+		);
 }
-add_action( 'wp_enqueue_scripts', 'alamilla_child_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'alamilla_enqueue_styles' );

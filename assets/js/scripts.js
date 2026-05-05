39e8da6a92b8c465be9e9c97174832b7ad24b536
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2026-05-05 11:09:52 -0400

    header, footer: reorganized code for show / hide functionality, added this functionality to footer, added theming to blend header and footer with the site theme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,8 +6,10 @@
 import themeObserver from "./src/themeObserver.js";
 import updateScroll from "./src/updateScroll.js";
 
+// Theming
 themeObserver();
 
+// Menu visibility
 window.addEventListener('scroll', () => {
     window.requestAnimationFrame(updateScroll);
 });
